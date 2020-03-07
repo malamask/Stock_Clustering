a785d0e64e4949b4992f6b61336b80d905fd7d49
Malamas <malamask@csd.auth.gr>
2020-03-07 20:05:28 +0200

    ver 1.2.1

--- Data_Script/stocks_auto_update.py
+++ Data_Script/stocks_auto_update.py
@@ -1,11 +1,14 @@
-import schedule
 import os
 import time
+import schedule
+
 
 def update():
     os.system("dataAPI.py")
 
-schedule.every(10).wednesday.at("12:00").do(update)
+
+schedule.every().wednesday.at("13:15").do(update)
+
 while 1:
     schedule.run_pending()
     time.sleep(1)
