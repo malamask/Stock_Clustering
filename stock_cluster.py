eacf3bb0fa4d876cf24c1d9c81c1c178be6b8b38
Malamas <malamask@csd.auth.gr>
2020-03-09 17:56:23 +0200

    ver 1.2.1

--- stock_cluster.py
+++ stock_cluster.py
@@ -7,8 +7,7 @@ import matplotlib.pyplot as plt
 import pandas_datareader.data as web
 
 
-
-
+'''
 path = r'C:\Users\coste\PycharmProjects\Stock_Clustering\Data_Script'
 stock_files = glob.glob(os.path.join(path,"*.csv"))
 df = pd.concat((pd.read_csv(f) for f in stock_files))
@@ -17,4 +16,7 @@ print(df.size)
 
 for a in df:
     print(df)
-    #print("malakies")
+'''
+
+
+
