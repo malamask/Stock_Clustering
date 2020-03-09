2d5c5e4d96df61e7f055f59606b57571a9ee5ca0
Malamas <malamask@csd.auth.gr>
2020-03-09 18:36:44 +0200

    ver 1.2.1

--- stock_cluster.py
+++ stock_cluster.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import pandas_datareader.data as web
 
 
-'''
+r"""
 path = r'C:\Users\coste\PycharmProjects\Stock_Clustering\Data_Script'
 stock_files = glob.glob(os.path.join(path,"*.csv"))
 df = pd.concat((pd.read_csv(f) for f in stock_files))
@@ -16,7 +16,21 @@ print(df.size)
 
 for a in df:
     print(df)
-'''
+"""
+
+
+path = r'C:\Users\coste\PycharmProjects\Stock_Clustering\Data_Script'
+
+stock_files = glob.glob(path + "/*.csv")
+
+stocks = []
+
+for filename in stock_files:
+    df = pd.read_csv(filename, index_col = None, header = 0)
+    stocks.append(df)
+
+frame = pd.concat(stocks, axis = 0, ignore_index = True)
+
 
 
 
