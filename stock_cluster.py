10ece7680c56b98c26ae6518f695b266b160c5a7
Malamas <malamask@csd.auth.gr>
2020-03-21 01:50:43 +0200

    ver 1.3.5 Hierarchical Clustering example

--- stock_cluster.py
+++ stock_cluster.py
@@ -77,7 +77,7 @@ main_df.dropna(axis=0, how='any', thresh=None, subset=None, inplace=False)
 main_df.to_csv('sp500_closes.csv')
 
 series_list = {} #use python dictionary
-
+series_listH = []
 #seperate the
 for symbol, value in main_df.iteritems():
     #print(key, value)
@@ -85,13 +85,17 @@ for symbol, value in main_df.iteritems():
     print()
     print(symbol)
     series_list[symbol] = np.array([], float)
-    for date, price in value[2:].items():
+    tempList = np.array([],float)
+    for date, price in value[2:].items(): #nan row and nan first values
         #series_list[]
+        tempList = np.append(tempList,price)
         print(f"Index : {date}, Value : {price}")
         series_list[symbol] = np.append(series_list[symbol],price)
     print(series_list[symbol])
+    series_listH.append(tempList)
 import itertools
 #start
+
 for series1, series2 in itertools.combinations(series_list, 2):
     #print(series1," ", series_list[series1])
     #print("kai")
@@ -125,6 +129,53 @@ for series in series_list:
 
     distance, path = fastdtw(x, y, dist=euclidean)
 
-    #print(distance)
-    # print(path)
+    print(distance)
+    print(path)
 """
+
+#hierarchical clustering
+import numpy as np
+
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram
+from sklearn.cluster import AgglomerativeClustering
+
+
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack([model.children_, model.distances_,
+                                      counts]).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+
+
+
+
+series_listH = np.asarray(series_listH,dtype=np.float32)
+print(series_listH)
+
+# setting distance_threshold=0 ensures we compute the full tree.
+model = AgglomerativeClustering(distance_threshold=0, n_clusters=None)
+
+model = model.fit(series_listH)
+#model = model.fit(X
+plt.title('Hierarchical Clustering Dendrogram')
+# plot the top three levels of the dendrogram
+plot_dendrogram(model, truncate_mode='level', p=6)
+plt.xlabel("Number of points in node (or index of point if no parenthesis).")
+plt.show()
+
