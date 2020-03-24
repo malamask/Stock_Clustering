ab1a68f53bb18d8a27f8f1d79a43a58d0ec2a780
Malamas <malamask@csd.auth.gr>
2020-03-24 22:02:13 +0200

    ver 1.4.2 Kmeans example

--- stock_cluster.py
+++ stock_cluster.py
@@ -25,6 +25,8 @@ from matplotlib import pyplot as plt
 
 
 #end
+from tslearn.clustering import TimeSeriesKMeans
+
 r"""
 path = r'C:\Users\coste\PycharmProjects\Stock_Clustering\Data_Script'
 stock_files = glob.glob(os.path.join(path,"*.csv"))
@@ -59,7 +61,7 @@ print(df.head())
 
 main_df = pd.DataFrame()
 for filename in stock_files:
-    df = pd.read_csv(filename,nrows=10)
+    df = pd.read_csv(filename,nrows=30)
 
     df.set_index('timestamp', inplace=True)
 
@@ -110,10 +112,10 @@ warnings.filterwarnings('ignore')
 import matplotlib.pylab as plt
 
 #%pylab inline
-from pyFTS.partitioners import CMeans, Grid, FCM, Huarng, Entropy, Util as pUtil
-from pyFTS.common import Membership as mf
-from pyFTS.benchmarks import benchmarks as bchmk
-from pyFTS.data import Enrollments
+#from pyFTS.partitioners import CMeans, Grid, FCM, Huarng, Entropy, Util as pUtil
+#from pyFTS.common import Membership as mf
+#from pyFTS.benchmarks import benchmarks as bchmk
+#from pyFTS.data import Enrollments
 
 
 
@@ -226,6 +228,105 @@ plot_dendrogram(model, truncate_mode='level', p=6)
 plt.xlabel("Number of points in node (or index of point if no parenthesis).")
 plt.show()
 
+#K-means
+
+"""
+from tslearn.generators import random_walks
+X = random_walks(n_ts=50, sz=32, d=1)
+print(X)
+print(series_listH)
+km = TimeSeriesKMeans(n_clusters=10, metric="euclidean", max_iter=30,random_state=0).fit(series_listH)
+#km = TimeSeriesKMeans(n_clusters=3, metric="euclidean", max_iter=5,random_state=0).fit(X)
+
+km.cluster_centers_.shape
+#print(series_listH)
+
+
+"""
+
+
+import numpy
+import matplotlib.pyplot as plt
+
+from tslearn.clustering import TimeSeriesKMeans
+from tslearn.datasets import CachedDatasets
+from tslearn.preprocessing import TimeSeriesScalerMeanVariance, \
+    TimeSeriesResampler
+
+seed = 0
+numpy.random.seed(seed)
+X_train, y_train, X_test, y_test = CachedDatasets().load_dataset("Trace")
+X_train = X_train[y_train < 4]  # Keep first 3 classes
+numpy.random.shuffle(X_train)
+# Keep only 50 time series
+X_train = TimeSeriesScalerMeanVariance().fit_transform(X_train[:50])
+# Make time series shorter
+X_train = TimeSeriesResampler(sz=40).fit_transform(X_train)
+sz = X_train.shape[1]
+
+# Euclidean k-means
+print("Euclidean k-means")
+km = TimeSeriesKMeans(n_clusters=3, verbose=True, random_state=seed)
+y_pred = km.fit_predict(X_train)
+
+plt.figure()
+for yi in range(3):
+    plt.subplot(3, 3, yi + 1)
+    for xx in X_train[y_pred == yi]:
+        plt.plot(xx.ravel(), "k-", alpha=.2)
+    plt.plot(km.cluster_centers_[yi].ravel(), "r-")
+    plt.xlim(0, sz)
+    plt.ylim(-4, 4)
+    plt.text(0.55, 0.85,'Cluster %d' % (yi + 1),
+             transform=plt.gca().transAxes)
+    if yi == 1:
+        plt.title("Euclidean $k$-means")
+
+# DBA-k-means
+print("DBA k-means")
+dba_km = TimeSeriesKMeans(n_clusters=3,
+                          n_init=2,
+                          metric="dtw",
+                          verbose=True,
+                          max_iter_barycenter=10,
+                          random_state=seed)
+y_pred = dba_km.fit_predict(X_train)
+
+for yi in range(3):
+    plt.subplot(3, 3, 4 + yi)
+    for xx in X_train[y_pred == yi]:
+        plt.plot(xx.ravel(), "k-", alpha=.2)
+    plt.plot(dba_km.cluster_centers_[yi].ravel(), "r-")
+    plt.xlim(0, sz)
+    plt.ylim(-4, 4)
+    plt.text(0.55, 0.85,'Cluster %d' % (yi + 1),
+             transform=plt.gca().transAxes)
+    if yi == 1:
+        plt.title("DBA $k$-means")
+
+# Soft-DTW-k-means
+print("Soft-DTW k-means")
+sdtw_km = TimeSeriesKMeans(n_clusters=3,
+                           metric="softdtw",
+                           metric_params={"gamma": .01},
+                           verbose=True,
+                           random_state=seed)
+y_pred = sdtw_km.fit_predict(X_train)
+
+for yi in range(3):
+    plt.subplot(3, 3, 7 + yi)
+    for xx in X_train[y_pred == yi]:
+        plt.plot(xx.ravel(), "k-", alpha=.2)
+    plt.plot(sdtw_km.cluster_centers_[yi].ravel(), "r-")
+    plt.xlim(0, sz)
+    plt.ylim(-4, 4)
+    plt.text(0.55, 0.85,'Cluster %d' % (yi + 1),
+             transform=plt.gca().transAxes)
+    if yi == 1:
+        plt.title("Soft-DTW $k$-means")
+
+plt.tight_layout()
+plt.show()
 
 
 
