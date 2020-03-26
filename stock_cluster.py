1ff09706c235edeac4bcd04d8164d21cd18dad08
Malamas <malamask@csd.auth.gr>
2020-03-26 02:52:41 +0200

    ver 1.4.3 Kmeans example

--- stock_cluster.py
+++ stock_cluster.py
@@ -61,7 +61,7 @@ print(df.head())
 
 main_df = pd.DataFrame()
 for filename in stock_files:
-    df = pd.read_csv(filename,nrows=30)
+    df = pd.read_csv(filename,nrows=70)#number of daily prices
 
     df.set_index('timestamp', inplace=True)
 
@@ -102,9 +102,6 @@ series_listH = np.asarray(series_listH,dtype=np.float32)
 print(series_listH)
 
 
-
-
-
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -142,7 +139,7 @@ for dataset in series_listH:
 
 
 
-#start
+#start - DTW only
 import itertools
 
 for series1, series2 in itertools.combinations(series_list, 2):
@@ -255,78 +252,65 @@ from tslearn.preprocessing import TimeSeriesScalerMeanVariance, \
 
 seed = 0
 numpy.random.seed(seed)
+print(seed)
 X_train, y_train, X_test, y_test = CachedDatasets().load_dataset("Trace")
-X_train = X_train[y_train < 4]  # Keep first 3 classes
-numpy.random.shuffle(X_train)
-# Keep only 50 time series
-X_train = TimeSeriesScalerMeanVariance().fit_transform(X_train[:50])
+print(X_train)
+#X_train = X_train[y_train < 4]  # Keep first 3 classes
+X_train = series_listH
+print(X_train)
+#numpy.random.shuffle(X_train)
+# Keep only 30 time series
+X_train = TimeSeriesScalerMeanVariance().fit_transform(X_train[:30])
 # Make time series shorter
 X_train = TimeSeriesResampler(sz=40).fit_transform(X_train)
+print(X_train.shape)
 sz = X_train.shape[1]
-
+print(sz)
 # Euclidean k-means
 print("Euclidean k-means")
-km = TimeSeriesKMeans(n_clusters=3, verbose=True, random_state=seed)
+km = TimeSeriesKMeans(n_clusters=3, verbose=True, random_state=0)
 y_pred = km.fit_predict(X_train)
-
 plt.figure()
 for yi in range(3):
-    plt.subplot(3, 3, yi + 1)
+    plt.subplot(1, 3, yi + 1)
     for xx in X_train[y_pred == yi]:
         plt.plot(xx.ravel(), "k-", alpha=.2)
     plt.plot(km.cluster_centers_[yi].ravel(), "r-")
     plt.xlim(0, sz)
-    plt.ylim(-4, 4)
+    plt.ylim(0, 200)
     plt.text(0.55, 0.85,'Cluster %d' % (yi + 1),
              transform=plt.gca().transAxes)
     if yi == 1:
         plt.title("Euclidean $k$-means")
 
-# DBA-k-means
-print("DBA k-means")
-dba_km = TimeSeriesKMeans(n_clusters=3,
-                          n_init=2,
-                          metric="dtw",
-                          verbose=True,
-                          max_iter_barycenter=10,
-                          random_state=seed)
-y_pred = dba_km.fit_predict(X_train)
-
-for yi in range(3):
-    plt.subplot(3, 3, 4 + yi)
-    for xx in X_train[y_pred == yi]:
-        plt.plot(xx.ravel(), "k-", alpha=.2)
-    plt.plot(dba_km.cluster_centers_[yi].ravel(), "r-")
-    plt.xlim(0, sz)
-    plt.ylim(-4, 4)
-    plt.text(0.55, 0.85,'Cluster %d' % (yi + 1),
-             transform=plt.gca().transAxes)
-    if yi == 1:
-        plt.title("DBA $k$-means")
-
-# Soft-DTW-k-means
-print("Soft-DTW k-means")
-sdtw_km = TimeSeriesKMeans(n_clusters=3,
-                           metric="softdtw",
-                           metric_params={"gamma": .01},
-                           verbose=True,
-                           random_state=seed)
-y_pred = sdtw_km.fit_predict(X_train)
-
-for yi in range(3):
-    plt.subplot(3, 3, 7 + yi)
-    for xx in X_train[y_pred == yi]:
-        plt.plot(xx.ravel(), "k-", alpha=.2)
-    plt.plot(sdtw_km.cluster_centers_[yi].ravel(), "r-")
-    plt.xlim(0, sz)
-    plt.ylim(-4, 4)
-    plt.text(0.55, 0.85,'Cluster %d' % (yi + 1),
-             transform=plt.gca().transAxes)
-    if yi == 1:
-        plt.title("Soft-DTW $k$-means")
 
 plt.tight_layout()
 plt.show()
 
 
 
+#test area
+"""
+X_train, y_train, X_test, y_test = CachedDatasets().load_dataset("Trace")
+X_train = series_listH
+print(X_train.size)
+X_train = TimeSeriesScalerMeanVariance().fit_transform(X_train[:10])
+X_train = X_train[y_train < 4]  # Keep first 3 classes
+X_train = series_listH
+print(X_train)
+numpy.random.shuffle(X_train)
+# Keep only 50 time series
+X_train = TimeSeriesScalerMeanVariance().fit_transform(X_train[:20])
+
+X_train, y_train, X_test, y_test = CachedDatasets().load_dataset("Trace")
+print(X_train)
+X_train = TimeSeriesScalerMeanVariance().fit_transform(X_train[:3])
+print(X_train)
+print(X_train.shape)
+X_train = series_listH
+
+
+X_train = TimeSeriesResampler(sz=10).fit_transform(X_train)
+print(X_train.shape)
+
+"""
